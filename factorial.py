adae9ec6e1cdced6e48d8a781312279d134e7713
= <=>
2025-01-08 11:25:20 +0530

    Taking number from user for factorial

--- factorial.py
+++ factorial.py
@@ -1,5 +1,9 @@
-n = 10
-mul = 1
-for i in range(1,n+1):
-	mul *= i
-print(mul)
+def factorial(n):
+	mul = 1
+	for i in range(1,n+1):
+		mul *= i
+	return mul
+
+if __name__ == "__main__":
+	n = int(input("Enter a number to find its factorial: "))
+	print(f"factorial of {n} = {factorial(n)}")
