eefd726c50c0773791653d97d344d5130f0cc358
= <=>
2025-01-08 11:42:32 +0530

    comment added

--- factorial.py
+++ factorial.py
@@ -1,5 +1,10 @@
-n = 6
-mul = 1
-for i in range(1,n+1):
-	mul *= i
-print(f"factorial of {n} is",mul)
+def factorial(n):
+	mul = 1
+	for i in range(1,n+1):
+		mul *= i
+	return mul
+
+if __name__ == "__main__":
+	#Taking input from user
+	n = int(input("Enter a number to find its factorial: "))
+	print(f"factorial of {n} = {factorial(n)}")
